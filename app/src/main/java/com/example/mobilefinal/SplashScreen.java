082ebd3fda97559b9a12c26512bc5ca751b70372
Tyreum <gabriel.kmn@gmail.com>
2020-07-11 21:02:41 -0300

    Spĺash e Login feito

---
+++ app/src/main/java/com/example/mobilefinal/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.mobilefinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 6000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
