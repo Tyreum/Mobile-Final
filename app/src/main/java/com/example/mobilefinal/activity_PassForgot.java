d9249a3ad415e1f07245a5157beb830166f84130
Pedro Lima <pedro.l2002@hotmail.com>
2020-07-13 00:15:23 -0300

    Tela cadastro e esqueceu senha feitas

---
+++ app/src/main/java/com/example/mobilefinal/activity_PassForgot.java
@@ -0,0 +1,14 @@
+package com.example.mobilefinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_PassForgot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_forgot);
+    }
+}
