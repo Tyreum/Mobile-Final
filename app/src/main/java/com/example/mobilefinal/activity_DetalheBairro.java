4934db3134419d7f73efafa06fa07a6d662a1d0d
guilhermemorningstar <guilhermemorningstar@yahoo.com>
2020-07-13 14:12:35 -0300

    Falta o mapa da tela de bairro expandido

--- app/src/main/java/com/example/mobilefinal/activity_DetalheBairro.java
+++ app/src/main/java/com/example/mobilefinal/activity_DetalheBairro.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_ListaAlimentos extends AppCompatActivity {
+public class activity_DetalheBairro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_lista_alimentos);
+        setContentView(R.layout.layout_detalhe_bairro);
     }
 }
