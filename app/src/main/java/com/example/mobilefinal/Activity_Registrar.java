ffa622f2c31a277fbb5ceb0aa0035444277a4907
Tyreum <gabriel.kmn@gmail.com>
2020-07-26 01:01:30 -0300

    fazendo as coisa funfa

--- app/src/main/java/com/example/mobilefinal/Activity_Registrar.java
+++ app/src/main/java/com/example/mobilefinal/Activity_Registrar.java
@@ -4,24 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Activity_Registrar extends AppCompatActivity {
     private Button btnRegister;
+    private EditText txtEmail;
+    private EditText txtSenha;
+
+    //API PEDRO
+    final RequestQueue queue = Volley.newRequestQueue(this);
+    final String url = "http://127.0.0.1:5000/cadastrar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
 
-        btnRegister = (Button) findViewById(R.id.btnNewRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity_Registrar();
-            }
-        });
     }
     public void openActivity_Registrar(){
         Intent intent = new Intent(this, Activity_Registrar.class);
