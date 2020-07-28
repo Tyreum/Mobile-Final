0f94665135503443e962e7f596c2c59bf86278a9
Tyreum <gabriel.kmn@gmail.com>
2020-07-28 11:51:39 -0300

    Lista dos Bairros ok

--- app/src/main/java/com/example/mobilefinal/Activity_DetalheBairro.java
+++ app/src/main/java/com/example/mobilefinal/Activity_DetalheBairro.java
@@ -2,29 +2,63 @@ package com.example.mobilefinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 public class Activity_DetalheBairro extends AppCompatActivity {
     private Button btnVoltar;
+    private TextView lstNomeBairros;
+    private TextView lstIdBairros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_detalhe_bairro);
 
+        //Config da Lista
+        lstIdBairros = (TextView) findViewById(R.id.idBairro);
+        lstNomeBairros = (TextView) findViewById(R.id.nomeBairro);
+
+        Intent intent = getIntent();
+        String nomeReturn = intent.getStringExtra("name");
+        String idReturn = intent.getStringExtra("id");
+
+        lstNomeBairros.setText(nomeReturn);
+        lstIdBairros.setText(idReturn);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Navegação
         btnVoltar = (Button) findViewById(R.id.btnVoltar);
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity_Home();
+                openActivity_ListaBairro();
             }
         });
     }
-    public void openActivity_Home(){
-        Intent intent = new Intent(this, Activity_Home.class);
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void openActivity_ListaBairro(){
+        Intent intent = new Intent(this, Activity_ListaBairro.class);
         startActivity(intent);
     }
 }
